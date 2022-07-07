5c3d6a160dbf26fa78598d359dafb56a9cb11ae3
Fabibourdokan <fabianabourdokanvaliente@gmail.com>
2022-07-07 18:46:10 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Fabiana"
+let number = 0311
+let year = 2022
+let seila = year - number
+ {console.log (year-number)}
+let dia = "quarta-feira"
+if ('true')
+ {console.log ('quarta-feira')} 
